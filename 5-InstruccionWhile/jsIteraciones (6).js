eb0dffcd771ceb42fa496e9191e19efedf326cb7
JulianVitacca18 <60760487+JulianVitacca18@users.noreply.github.com>
2020-02-20 22:12:56 -0300

    ejercicio 6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -2,11 +2,29 @@ function mostrar()
 {
 
 	var contador=0;
-	var acumulador=0;
+    var acumulador=0;
+	var promedio;
+	
+	
+	while (contador <5)
+	{
+		numero = parseInt (prompt + ("Ingrese un numero"));
+		
+	while (isNaN (numero)) 
+	{
+		numero = parseInt (prompt + ("No es un numero, ingrese un numero"));
+	}
+		
+		acumulador = acumulador + numero;
 
+		contador ++;
+	}
 
+	promedio = acumulador/5
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+    document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value=acumulador = promedio;
+
 
 }//FIN DE LA FUNCIÓN
+
