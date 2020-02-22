a84495ac50611aa159b220bf8ea393a94b82a1bc
JulianVitacca18 <julianvitaccast@gmail.com>
2020-02-21 22:10:34 -0300

    9 y 10 de while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,59 @@
 function mostrar()
 {
+	var acumulador_negativos;
+	var acumulador_positivos;
+	var contador_positivos;
+	var contador_negativos;
+	var contador_ceros;
+	var promedio_positivos;
+	var promedio_negativos;
+	var diferencia_positivos_negativos;
+	var numero;
+	var seguir;
+	var respuesta;
+	var positivo = 0;
+	var negativo = 1;
 
-	var contador=0;
-	//declarar contadores y variables 
 	
-	var respuesta="si";
-
-	while(respuesta!="no")
-	{
+	do {
+	
+		numero = prompt ("Ingrese aqui un numero");
+	
+	 while (isNaN (numero)) {
 		
+		numero = prompt ("Eso no es un numero, reingrese un numero");
+	 }
+
+	if ( numero >= 0) {
 	
+	acumulador_positivos = numero + positivo;
+	contador_positivos ++;
+
+	} 
+	if ( numero < 0) {
+	
+	acumulador_negativos = numero * negativo;
+	contador_negativos ++;
 	}
+	 
+	} while (seguir == respuesta);
+
+
+	document.write ("1_ La suma de los negativos es: " + acumulador_negativos + ("</br>"));
+	document.write ("2_ La suma de los positivos es: " + acumulador_positivos + ("</br>"));
 
 
 
 
 }//FIN DE LA FUNCIÓN
+
+/* Al presionar el botón pedir números hasta que el usuario quiera, mostrar:
+1-Suma de los negativos. 
+2-Suma de los positivos. 
+3-Cantidad de positivos. 
+4-Cantidad de negativos. 
+5-Cantidad de ceros. 
+6-Cantidad de números pares. 
+7-Promedio de positivos. 
+8-Promedios de negativos. 
+9-Diferencia entre positivos y negativos, (positvos-negativos).*/
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,38 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
+	var flag = 0;
+	var maximo;
+	var respuesta = "si";
+	var minimo;
+	var numero;
 	
-	var respuesta='si';
 
-	while(respuesta!='no')
-	{
-		
+	do {
+      numero = parseInt (prompt ("Ingrese un numero"));
 	
+    while (isNaN (numero) ){
+      numero = prompt ("Ese no es un numero, porfavor ingrese un numero"); 
+	}
+	 
+	if ( flag == 0 || numero > maximo) {
+	   maximo = numero;
+	}
+	
+	if ( flag == 0 || numero < minimo) {
+	   minimo = numero;
+	   flag = 1;
 	}
 
+    seguir = prompt ("¿Quiere ingresar otro numero?, ingrese si o no");
 
 
+	}while ( seguir == respuesta);
+		
+
+document.getElementById ("maximo").value = maximo;
+document.getElementById ("minimo").value = minimo;
+	
+	
 
 }//FIN DE LA FUNCIÓN
