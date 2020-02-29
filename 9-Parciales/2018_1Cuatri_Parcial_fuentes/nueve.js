632074d5cacd5762aa5781913aa0c65fc2918a75
JulianVitacca18 <60760487+JulianVitacca18@users.noreply.github.com>
2020-02-28 22:08:12 -0300

    parcial 8 y 9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+ var marca;
+ var peso;
+ var temperatura;
+ var cant_temperaturas_pares =0;
+ var marca_producto_mas_pesado;
+ var producto_menos_cero_grados;
+ var promedio_peso_productos=0;
+ var acumulador_peso =0;
+ var contador_peso =0;
+ var peso_max;
+ var peso_min;
+ var seguir;
 
-}
+//--------------------------
+
+do {
+
+    marca = prompt ("Ingrese la marca del producto: ");
+    peso = parseInt (prompt ("Ingrese el peso del producto (entre 1 y 100): "));
+    while (peso <1 || peso >100 ) {
+        peso = parseInt (prompt ("PESO INVALIDO. Ingrese el peso del producto (entre 1 y 100): "));
+    }
+
+
+
+
+
+} while ( seguir == "si");
+
+
+
+
+
+
+} //FIN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -2,20 +2,27 @@ function mostrar()
 {
    var letra;
    var numero;
-   var cant_num_par;
-   var cant_num_impar;
-   var cant_ceros;
-   var promedio_positivos;
-   var total_negativos;
+   var seguir;
+   var cant_num_par = 0;
+   var cant_num_impar = 0;
+   var contador_ceros =0;
+   var contador_positivos =0;
+   var contador_negativos =0;
+   var promedio_positivos =0;
+   var acumulador_negativos =0;
    var flag = 0;
    var respuesta = "si";
-   var acumulador;
+   var num_MAX;
+   var num_MIN;
+   var letra_MIN;
+   var letra_MAX;   
+   var acumulador_positivos =0;
    
-  /// var dato;
    //----------------------------------
+
 do {
    letra = prompt ("Ingrese una letra random");
-while (isNaN (letra) == false) {
+while (!((letra >= "A"&& letra <= "Z") || (letra >= "a" && letra <= "z"))) {
     letra = prompt ("No es una letra, reingrese una letra random");
 }
 
@@ -24,13 +31,64 @@ while (numero <-100 || numero >100 || isNaN (numero) ){
     numero = parseInt (prompt ("No es un numero valido, reingrese un numero random entre -100 y 100"));
 }
 
-respuesta = prompt ("¿Desea seguir? ingrese si o no")
+//----------NUMEROS PARES E IMPARES----------------
+if ((numero) % 2 == 0){
+    cant_num_par ++; 
+}
+else {
+    cant_num_impar ++;
+}
+
+//---------NUMEROS POSITIVOS, NEGATIVOS Y CEROS----------
+
+if (numero >0 ){
+    acumulador_positivos = acumulador_positivos + numero;
+    contador_positivos ++;
+
+}
+else if (numero <0){
+    acumulador_negativos = acumulador_negativos + numero;
+
+}
+else {
+    contador_ceros ++;
+}
+
+//--------LETRA Y NUMERO MAX Y MIN-----------
+
+if (flag == 0 || numero < num_MIN){
+    num_MIN = numero;
+    letra_MIN = letra;   
+}
+if (flag == 0 || numero > num_MAX){
+    num_MAX = numero;
+    letra_MAX = letra;   
+    flag = 1
+}
+
+seguir = prompt ("¿Desea seguir? ingrese si o no");
+while (!(seguir == "si" || seguir == "no" )) {
+    seguir = prompt ("DEBE INGRESAR si O no PARA CONTINUAR");
+}
 
 } while (seguir == respuesta);
 
+if (contador_positivos != 0) {
+    promedio_positivos = acumulador_positivos / contador_positivos ;
+
+}
+
+document.write ("a)  La cantidad de números pares es : " + cant_num_par + "</br>");
+document.write ("b)  La cantidad de números impares es : " + cant_num_impar + "</br>"); 
+document.write ("c)  La cantidad de ceros es : " + contador_ceros + "</br>");
+document.write ("d)  El promedio de todos los números positivos ingresados es : " + promedio_positivos + "</br>");
+document.write ("e)  La suma de todos los números negativos " + acumulador_negativos + "</br>");
+document.write ("f)  El número y la letra del máximo es " +  num_MAX + letra_MAX +  " y el mínimo " + num_MIN + letra_MIN  + "</br>");
+
+
 }//Fin de la funcion
 
-   
+
 
 
 
